5ffb2ecabef0a1c4e6a989b9e8c2795303c23415
shoyo hinata <jdoufare2001@gmail.com>
2024-10-27 10:37:26 +0100

    create a mini terraform project that stores state files in a remote storage

---
+++ 3.terraform-remote-state-management-example/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# data block => to get a specific ami of an ec2 instance
+data "aws_ami" "ec2_ami" {
+
+        most_recent = true                     # Fetch the most recent AMI
+        owners      = ["099720109477"]         # Canonical (official Ubuntu owner)
+
+        # to filter available amis based on name
+          filter {
+                name   = "name"
+                values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+             }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ec2_ami.id  # Use the fetched AMI ID
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "state-test-instance"
+  }
+}
